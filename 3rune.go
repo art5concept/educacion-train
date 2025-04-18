7cb3b0eee2ffe85f4256b2b0c485fb90328525fc
felix_lara <larafelix2197@gmail.com>
2025-04-18 09:41:16 -0500

    capitulo 1 aprendiendo go

---
+++ 3rune.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var r rune = '🐻'      // Asignar un carácter a una rune
+	fmt.Println(r)        // Imprime el valor numérico de la rune
+	fmt.Printf("%c\n", r) // Imprime el carácter correspondiente
+}
