90d9f12cc89037df5d697a2e59c6fceb7986e16e
felix_lara <larafelix2197@gmail.com>
2025-05-15 19:36:53 -0500

    type asertion and building a method

--- week3/23/main.go
+++ week3/23/main.go
@@ -22,8 +22,32 @@ type systemAlert struct {
 	messageContent string
 }
 
-// ?
+func (dm directMessage) importance() int {
+	if dm.isUrgent {
+		return 50
+	} else {
+		return dm.priorityLevel
+	}
+}
+
+func (gm groupMessage) importance() int {
+	return gm.priorityLevel
+}
+
+func (sa systemAlert) importance() int {
+	return 100
+}
 
 func processNotification(n notification) (string, int) {
-	// ?
+
+	switch x := n.(type) {
+	case directMessage:
+		return x.senderUsername, x.importance()
+	case groupMessage:
+		return x.groupName, x.importance()
+	case systemAlert:
+		return x.alertCode, x.importance()
+	default:
+		return "", 0
+	}
 }
